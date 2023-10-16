1b2131649ea6a6412124592e2eda9bcd66810767
DmitryKuz <dm.kuzmin11@mail.ru>
2023-10-16 15:52:47 +0300

    Initial commit

---
+++ GaussianMethod.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CalculationMethods_L3_WPF;
+
+public static class GaussianMethod
+{
+    // Метод Гаусса для решения системы линейных уравнений
+    public static string Gauss(double[,] system)
+    {
+        var n = 4; //разрядность системы
+        var result = new double[n];
+            
+        // Прямой ход метода Гаусса
+        for (var i = 0; i < n-1; i++)
+        {
+            for (var j = i+1; j < n; j++)
+            {
+                var ratio = system[j, i] / system[i, i];
+                    
+                for (var k = i; k < n; k++)
+                {
+                    system[j, k] -= ratio * system[i, k];
+                }
+            }
+        }
+            
+        // Обратный ход метода Гаусса
+        for (var i = n - 1; i >= 0; i--)
+        {
+            result[i] = system[i, n] / system[i, i];
+
+            for (var j = i - 1; j >= 0; j--)
+            {
+                system[j, n] -= system[j, i] * result[i];
+                system[j, i] = 0;
+            }
+        }
+
+        var output = "";
+        for (var i = 0; i < n; i++)
+        {
+            output += "x" + (i + 1) + " = " + result[i] + "\n";
+        }
+        return output;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static CalculationMethods_L3_WPF.GaussianMethod;
+
+namespace CalculationMethods_L3_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            double[,] system =
+            {
+                {Double.Parse(TBx1_1.Text.Replace('.', ',')), Double.Parse(TBx2_1.Text.Replace('.', ',')), 
+                    Double.Parse(TBx3_1.Text.Replace('.', ',')), Double.Parse(TBx4_1.Text.Replace('.', ',')), 
+                    Double.Parse(TBv1.Text.Replace('.', ','))},
+                {Double.Parse(TBx1_2.Text.Replace('.', ',')), Double.Parse(TBx2_2.Text.Replace('.', ',')), 
+                    Double.Parse(TBx3_2.Text.Replace('.', ',')), Double.Parse(TBx4_2.Text.Replace('.', ',')), 
+                    Double.Parse(TBv2.Text.Replace('.', ','))},
+                {Double.Parse(TBx1_3.Text.Replace('.', ',')), Double.Parse(TBx2_3.Text.Replace('.', ',')), 
+                    Double.Parse(TBx3_3.Text.Replace('.', ',')), Double.Parse(TBx4_3.Text.Replace('.', ',')),
+                    Double.Parse(TBv3.Text.Replace('.', ','))},
+                {Double.Parse(TBx1_4.Text.Replace('.', ',')), Double.Parse(TBx2_4.Text.Replace('.', ',')), 
+                    Double.Parse(TBx3_4.Text.Replace('.', ',')), Double.Parse(TBx4_4.Text.Replace('.', ',')), 
+                    Double.Parse(TBv4.Text.Replace('.', ','))}
+            };
+            
+            TbOutput.Text = Gauss(system);
+        }
+    }
+}
